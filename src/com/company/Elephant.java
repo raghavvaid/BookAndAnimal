163bd43cfb58893960a272eecc2625600914a704
adsf <asd>
2022-03-15 12:13:19 -0400

    asdf

--- src/com/company/Elephant.java
+++ src/com/company/Elephant.java
@@ -3,6 +3,6 @@ package com.company;
 public class Elephant extends Herbivore{
     private double length;
     public Elephant(double len, String name){
-
+        super("Elephant", name);
     }
 }
