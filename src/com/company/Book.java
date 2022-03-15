bb0a9623a3aa6b83c858210a9658a758cbb5b2a6
adsf <asd>
2022-03-15 10:50:56 -0400

    asdf

---
+++ src/com/company/Book.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Book {
+    private String title;
+    private String author;
+
+    public Book(String t, String a)
+    {
+        title = t;
+        author = a;
+    }
+
+    public void printBookInfo()
+    {
+        System.out.print(title + ", written by " + author);
+    }
+}
